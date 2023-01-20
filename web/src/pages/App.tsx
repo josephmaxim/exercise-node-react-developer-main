dacbd3b25622ea25ebae3a8e56ef0e5cc24fcf73
josephmaxim <joseph@plasmacreative.com>
2023-01-20 00:13:22 -0500

    Display fetched list of repository on the main page

--- web/src/pages/App.tsx
+++ web/src/pages/App.tsx
@@ -1,3 +1,21 @@
+import { useContext } from 'react';
+import { GlobalContext } from '../context/GlobalContext';
+
 export function App() {
-  return <>hello</>;
+  const { globalState } = useContext(GlobalContext);
+  const { repos } = globalState;
+
+  const displayRepos = repos.map((repo: any, key: string) => {
+    const { name, description, language, forks_count } = repo;
+    return (
+      <ul key={key}>
+        <li>Name: {name}</li>
+        <li>Description: {description}</li>
+        <li>Language: {language}</li>
+        <li>Forks Count: {forks_count}</li>
+      </ul>
+    );
+  });
+
+  return <main>{displayRepos}</main>;
 }
