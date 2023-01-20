48c56d5455a726be2f6bf0cb4f8f5c063a8ed922
josephmaxim <joseph@plasmacreative.com>
2023-01-19 21:10:30 -0500

    Add Axios npm package and fetch data from github with error handling.

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -1,4 +1,6 @@
 import { Router, Request, Response } from 'express';
+import { AppError } from '../models/AppError';
+import axios from 'axios';
 
 export const repos = Router();
 
@@ -8,5 +10,13 @@ repos.get('/', async (_: Request, res: Response) => {
   res.status(200);
 
   // TODO: See README.md Task (A). Return repo data here. You’ve got this!
-  res.json([]);
+  try {
+    const { data } = await axios.get(
+      'https://api.github.com/users/silverorange/repos'
+    );
+    return res.json(data);
+  } catch (err: any) {
+    const error: AppError = new AppError(err.response.data.message, 500);
+    return res.status(error.status).json(err.response.data);
+  }
 });
