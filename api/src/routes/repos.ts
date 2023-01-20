0e15bc13c89283695ae7f912348420be1a71fa03
josephmaxim <joseph@plasmacreative.com>
2023-01-19 22:13:23 -0500

    Add local json data source to /repo endpoint as default source

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -1,5 +1,8 @@
 import { Router, Request, Response } from 'express';
+import {promises as fs} from 'fs';
+import path from 'path';
 import { AppError } from '../models/AppError';
+import { Repo } from '../models/Repo';
 import axios from 'axios';
 
 export const repos = Router();
@@ -10,10 +13,38 @@ repos.get('/', async (_: Request, res: Response) => {
   res.status(200);
 
   // TODO: See README.md Task (A). Return repo data here. You’ve got this!
-  try {
+
+  // (A) 2. I wasn't sure if im suppose to join this two data source so I decided to
+  // add a query flag that accepts a string value of: local
+  // if source query isn't present just fetch github data
+
+  const {
+    query: { source },
+  } = _;
+
+  const getLocalData = async () => {
+    let fileData: string | any[] = '';
+    fileData = await fs.readFile(path.resolve('data', 'repos.json'), 'utf8');
+    return JSON.parse(fileData);
+  };
+
+  const getGithubData = async () => {
     const { data } = await axios.get(
       'https://api.github.com/users/silverorange/repos'
     );
+    return data;
+  };
+
+  let data: any = {};
+
+  // determines the data source to use;
+  try {
+    if (source === 'github') {
+      data = await getGithubData();
+    } else {
+      data = await getLocalData();
+    }
+
     return res.json(data);
   } catch (err: any) {
     const error: AppError = new AppError(err.response.data.message, 500);
