38330c821f7f9151795df915c1d74aa17f39d11c
josephmaxim <joseph@plasmacreative.com>
2023-01-19 22:42:36 -0500

    Implement Sass for styling.

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import { App } from './pages/App';
 import { reportWebVitals } from './reportWebVitals';
 
+import './scss/root.scss';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
